6aa38526a4e1e47fb77362b7f09a1ef5ba38e389
JT <jttyburski@gmail.com>
2015-05-11 16:20:25 +0200

    MY

--- app/src/main/java/com/example/jt/nfcaplikacja4/MainActivity.java
+++ app/src/main/java/com/example/jt/nfcaplikacja4/MainActivity.java
@@ -212,6 +212,12 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
+            case 5:
+                mTitle = getString(R.string.title_section5);
+                break;
         }
     }
 
--- app/src/main/java/com/example/jt/nfcaplikacja4/Sets.java
+++ app/src/main/java/com/example/jt/nfcaplikacja4/Sets.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RunningHistory extends ActionBarActivity {
+public class Sets extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_running_history);
+        setContentView(R.layout.activity_sets);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_running_history, menu);
+        getMenuInflater().inflate(R.menu.menu_sets, menu);
         return true;
     }
 
